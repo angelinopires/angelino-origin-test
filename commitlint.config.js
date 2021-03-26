59186765bb680c5f6d059476d987d5579fb02e43
Angelino Oliveira <lino.pires@live.com>
2021-03-26 16:46:22 -0300

    chore: config the linter

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'scope-enum': [2, 'always', ['simulation']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
+};
+
+

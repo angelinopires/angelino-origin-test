73309b435bc962a6549c519cbfb7d99e2e913444
Angelino Oliveira <lino.pires@live.com>
2021-04-01 12:57:10 -0300

    test: fix the tests config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,21 @@
 module.exports = {
   bail: 1,
+  clearMocks: true,
   collectCoverage: true,
+  coverageDirectory: "coverage",
   moduleDirectories: ['node_modules', '<rootDir>/src'],
   moduleFileExtensions: ['tsx', 'ts', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    '.+\\.scss$': '<rootDir>/__mocks__/styleMock.ts',
+    '.+\\.s?css$': '<rootDir>/__mocks__/styleMock.ts',
+    "~(.*)$": "<rootDir>/src/$1"
   },
   preset: 'ts-jest',
-  roots: ['<rootDir>/src'],
+  roots: [
+    "<rootDir>/src"
+  ],
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/'],
   testRegex: '.(test|spec).(jsx?|tsx?)$',
   transform: {
     '^.+\\.(js|ts)x?$': 'ts-jest',
   },
-  verbose: true,
-}
+};
